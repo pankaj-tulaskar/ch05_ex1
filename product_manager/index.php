86fa4695fec25e1c322bcdeb5820e6ec61f1963d
pankaj s tulaskar <pst28@afsconnect1.njit.edu>
2017-03-05 19:56:33 -0500

    edited action

--- product_manager/index.php
+++ product_manager/index.php
@@ -63,5 +63,20 @@ if ($action == 'list_products') {
         add_product($category_id, $code, $name, $price);
         header("Location: .?category_id=$category_id");
     }
-}    
+}  else if ($action == 'add_category') {
+    
+    $name = filter_input(INPUT_POST, 'name');
+    if ($name == NULL) {
+    $error = "INPUT data.";
+        include('../errors/error.php');  
+    }  else {
+          add_category($name);
+          header("Location: .?action=list_categories");
+          }
+}  else if ($action == 'delete_category') {
+          $category_id = filter_input(INPUT_POST, 'category_id');
+          delete_category($category_id);
+          header("Location: .?action=list_categories");
+}
+    
 ?>
